d18ee4f5143b9a894269fd135e91d79ab0e2e05c
MeNadhif <nadhif1495@gmail.com>
2023-11-27 22:27:53 +0700

    Create Car Catalogue

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Navbar, Hero, CustomButton, Footer };
+export {
+   Navbar,
+   Hero,
+   CustomButton,
+   Footer,
+   CustomFilter,
+   SearchBar,
+   SearchManufacturer,
+};
